8036f66e7aa32ae9a46b0b5c22cb6e0283e46358
Ming Yan <mingyan214@gmail.com>
2015-11-09 23:59:19 +1100

    Adds a few dependencies

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 /**
  * Created by mingyan on 7/11/15.
  */
-console.log('hello world')
+var React = require('react');
+var styles = require('./styles/main.styl');
+
+var Hello = React.createClass({
+  displayName: 'Hello',
+  render: function(){
+    return <div>Hi, {this.props.name}</div>
+  }
+})
+
+React.render(<Hello name='World!'/>, document.body);
