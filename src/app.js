a243df82dd82a4b54663aa6cc2b83ea248651102
Ming Yan <mingyan214@gmail.com>
2015-11-10 23:57:31 +1100

    Adds Stylus and yeticss

--- src/app.js
+++ src/app.js
@@ -7,7 +7,38 @@ var styles = require('./styles/main.styl');
 var Hello = React.createClass({
   displayName: 'Hello',
   render: function(){
-    return <div>Hi, {this.props.name}</div>
+    return (<div>Hi, {this.props.name}
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Surname</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Adam</td>
+            <td>Brault</td>
+            <td>adam@andyet.com</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Lynn</td>
+            <td>Fisher</td>
+            <td>lynn@andyet.com</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>Adam</td>
+            <td>Baldwin</td>
+            <td>baldwin@andyet.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>)
   }
 })
 
